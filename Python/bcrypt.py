2708d4d9fc1cbc70adf22a11aef92b3a5e5be0fd
Spider_Code <peeratach.ten@gmail.com>
2022-09-18 23:34:52 +0700

    edit for version

--- Python/bcrypt.py
+++ Python/bcrypt.py
@@ -1,12 +1,13 @@
+# python version 2.6.15 and less than
+
 import bcrypt
 
-password = b"padmin10!2a"
-
-hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-
+my_password = bcrypt.hashpw('peter110!', bcrypt.gensalt)
+print(my_password)
+decrypt = bcrypt.hashpw('peter110!', my_password)
+print(decrypt)
 # if bcrypt.checkpw(password, hashed):
 #     print("It Matches!")
 # else:
 #     print("It Does not Match :(")
 
-print(hashed)
